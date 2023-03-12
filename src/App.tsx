17e791cf735bfedf94643277d32e8f7bed3a3bba
yahia <mohamedyahia207545>
2023-03-12 04:21:49 +0200

    todomodel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.Model";
 
 const App: FC = () => {
-  const todos = [{ id: "t1", text: "Finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos([{ id: Math.random.toString(), text: text }]);
+  };
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
